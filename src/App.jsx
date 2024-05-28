3f02b83d49eeaa60dabb698f7ace0ed08951ce39
Ezequiel Martinez <48684675+ezemartlopez@users.noreply.github.com>
2024-05-28 15:31:02 -0300

    altura y tamaño del principal

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import PanelTasks from "@appcomponents/PanelTasks";
 export default function App() {
   const {toggleMode, mode} = UseMode();
   return (
-    <div className={"w-screen h-full " + (mode? "dark": "")}>
+    <div className={"w-screen min-h-screen " + (mode? "dark": "")}>
       <div className="relative size-full dark:bg-slate-900 bg-slate-100 transition-colors ease-in duration-500">
         <div className="absolute z-0 w-full h-[300px] bg-purple-800 ">
           <BackGroundImage modeActive={mode}/>
         </div>
-        <main className="absolute z-10 w-full px-4 sm:px-8 h-full flex justify-center">
+        <main className="absolute z-10 w-full px-4 pb-4 sm:px-8 h-full flex justify-center">
           <div className="sm:w-[540px] w-full h-full pt-16 flex flex-col">
             <Navbar toggle={toggleMode} modeActive={mode}/>
             <PanelTasks/>
