6ee52ee130294a82d3e594a5e5678c4579c0af53
Ezequiel Martinez <48684675+ezemartlopez@users.noreply.github.com>
2024-05-28 15:23:21 -0300

    prueba de modificacion a ver si era el problema.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PanelTasks from "@appcomponents/PanelTasks";
 export default function App() {
   const {toggleMode, mode} = UseMode();
   return (
-    <div className={"w-screen h-screen " + (mode? "dark": "")}>
+    <div className={"w-screen h-full " + (mode? "dark": "")}>
       <div className="relative size-full dark:bg-slate-900 bg-slate-100 transition-colors ease-in duration-500">
         <div className="absolute z-0 w-full h-[300px] bg-purple-800 ">
           <BackGroundImage modeActive={mode}/>
